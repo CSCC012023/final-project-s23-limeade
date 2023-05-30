27905527ecb9ff8711fac3f5c718ce7023d706fc
youngjaeheo2002 <youngjaeheo2002@gmail.com>
2023-05-30 11:27:52 -0400

    Revert "feat: environment"
    This reverts commit 90646e542339777d17c203aca61ab3ff552b2c82.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './pages/index/index.component';
-const routes: Routes = [
-  {
-    path:'',
-    component:IndexComponent
-  }
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
