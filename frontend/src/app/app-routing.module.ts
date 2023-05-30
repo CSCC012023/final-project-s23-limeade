90646e542339777d17c203aca61ab3ff552b2c82
youngjaeheo2002 <youngjaeheo2002@gmail.com>
2023-05-30 11:16:25 -0400

    feat: environment

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { IndexComponent } from './pages/index/index.component';
+const routes: Routes = [
+  {
+    path:'',
+    component:IndexComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
